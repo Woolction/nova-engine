6a583b97cbed6478a15beb9ccebbc9014e29e5ce
Zynres <killp8918@gmail.com>
2026-07-27 21:55:18 +0300

    fix: delete lyfecycle realization from mixer manager

--- src/SweetEngine/MDI/MixerManager.cs
+++ src/SweetEngine/MDI/MixerManager.cs
@@ -1,41 +1,27 @@
+using SweetLib.Collections.Unsafe.List;
+using SweetEngine.Core.APIs;
+using SweetEngine.Core;
+
 namespace SweetEngine.MDI;
 
 /// <summary>
 ///     the manager responsible for oberseeing mixers and the lifecycle.
 /// </summary>
-public struct MixerManager
+public unsafe struct MixerManager
 {
-    public void Prep()
+    public LifeCycle Life;
+
+    public MixerManager()
     {
-        
+        Life = new();
     }
 
-    public void Enable()
+    public void Init()
     {
-        
+    
     }
-
-    public void Fill()
-    {
-        
-    }
-
-    public void Whip()
-    {
-        
-    }
-
-    public void Blend()
-    {
-        
-    }
-
-    public void Disable()
-    {
-        
-    }
-
-    public void Consume()
+    
+    public void Dispose()
     {
         
     }
