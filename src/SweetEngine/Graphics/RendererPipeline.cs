2dfc90d133203fe942ba778f083ad7024f333083
Zynres <killp8918@gmail.com>
2026-07-27 21:43:21 +0300

    feat: add renderer pipeline OpenGL implement wrapper

--- src/SweetEngine/Graphics/RendererPipeline.cs
+++ src/SweetEngine/Graphics/RendererPipeline.cs
@@ -1,6 +1,42 @@
+using SweetEngine.Core;
+using SweetEngine.Core.APIs;
+
 namespace SweetEngine.Graphics;
 
-public struct RenderPipeline
+public unsafe struct RenderPipeline
 {
+    private RenderingAPI API;
     
+    public void Init(in EngineContext context)
+    {
+        API = new RenderingAPI() { 
+            Init = &OpenGL.Init,
+            Before = &OpenGL.Before,
+            AfterBefore = &OpenGL.AfterBefore,
+            Render = &OpenGL.Render,
+            After = &OpenGL.After
+        };
+
+        API.Init(in context);
+    }
+
+    public void Before(in EngineContext context)
+    {
+        API.Before(in context);
+    }
+
+    public void AfterBefore(in EngineContext context)
+    {
+        API.AfterBefore(in context);
+    }
+
+    public void Render(in EngineContext context)
+    {
+        API.Render(in context);
+    }
+
+    public void After(in EngineContext context)
+    {
+        API.After(in context);
+    }
 }
