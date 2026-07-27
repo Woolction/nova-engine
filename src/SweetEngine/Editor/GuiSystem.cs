a5aac2eec7b88d55bb7f1fdc796d722bc7633b4b
Zynres <killp8918@gmail.com>
2026-07-27 21:41:37 +0300

    feat: add gui systme imgui implement wrapper

--- src/SweetEngine/Editor/GuiSystem.cs
+++ src/SweetEngine/Editor/GuiSystem.cs
@@ -1,6 +1,31 @@
+using SweetEngine.Core.APIs;
+using SweetEngine.Core;
+
 namespace SweetEngine.Editor;
 
-public struct GuiSystem
+public unsafe struct GuiSystem
 {
-    
+    private GuiAPI API;
+
+    public void Init(in EngineContext context)
+    {
+        API = new GuiAPI()
+        { 
+            Init = &ImGUI.Init,
+            Update = &ImGUI.Update,
+            Render = &ImGUI.Render
+        };  
+
+        API.Init(in context);
+    }
+
+    public readonly void Update(in EngineContext context)
+    {
+        API.Update(in context);
+    }
+
+    public void Render(in EngineContext context)
+    {
+        API.Render(in context);
+    }
 }
