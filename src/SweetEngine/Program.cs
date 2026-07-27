ec20c1c80e3f44edf4ef3bb768939e582bcb31f3
Zynres <killp8918@gmail.com>
2026-07-27 21:57:01 +0300

    implement: add all changes to the program

--- src/SweetEngine/Program.cs
+++ src/SweetEngine/Program.cs
@@ -2,9 +2,10 @@
 // Licensed under the Apache-2.0 License.
 
 using System.Runtime.InteropServices;
+using SweetEngine.MDI.Ingredients;
 using SweetEngine.Editor.Windows;
 using SweetEngine.Core.Enums;
-using SweetEngine.IO.Loaders;
+using SweetEngine.MDI.Mixers;
 using SweetEngine.Resources;
 using SweetEngine.Graphics;
 using SweetLib.Devices;
@@ -21,8 +22,11 @@ unsafe class Program
         try
         {
             var engine = new Engine();
+            var context = new EngineContext(
+                &engine.Device, &engine.Intent, &engine.Kitchen, 
+                &engine.Mixer, &engine.Resource, &engine.Editor);
 
-            engine.Init();
+            engine.Init(in context);
 
             var window = GraphicContext.Window;
             var glfw = GraphicContext.Glfw;
@@ -31,38 +35,61 @@ unsafe class Program
             DebugWindow.Window = &engine.Device.Window;
             DebugWindow.Mouse = &engine.Device.Mouse;
 
-            var textureLoader = new Texture2DLoader();
-            var rendering = new OpenGLRenderer(&engine.Device, in textureLoader);
-
-            var _baseMap = textureLoader.Load(TextureType.BaseMap, AssetDirectories.Textures + "/sakuya-Base_Color.png");
-            var _normalMap = textureLoader.Load(TextureType.NormalMap, AssetDirectories.Textures + "/sakuya-Normal.png");
-            var _metallicMap = textureLoader.Load(TextureType.MetallicMap, AssetDirectories.Textures + "/sakuya-Metallic.png");
+            var _baseMap = context.Resource->TextureLoader.Load(TextureType.BaseMap, AssetDirectories.Textures + "/sakuya-Base_Color.png");
+            var _normalMap = context.Resource->TextureLoader.Load(TextureType.NormalMap, AssetDirectories.Textures + "/sakuya-Normal.png");
+            var _metallicMap = context.Resource->TextureLoader.Load(TextureType.MetallicMap, AssetDirectories.Textures + "/sakuya-Metallic.png");
 
             Material* mat = (Material*)NativeMemory.Alloc((nuint)sizeof(Material));
-            *mat = new Material(in textureLoader);
+            *mat = new Material(in context.Resource->TextureLoader);
 
             mat->Textures.Set(0, _baseMap);
             mat->Textures.Set(1, _normalMap);
             mat->Textures.Set(2, _metallicMap);
             mat->Color = new Vector4(1, 1, 1, 1);
 
-            rendering.AddObject(AssetDirectories.Models + "/NewSakuya.obj", mat);
-            //rendering.AddObject(AssetDirectories.Models + "/cube.obj", mat);
-            rendering.InitializeObjects();
+            engine.Kitchen.CurrentKitchen.AddDessert(AssetDirectories.Models + "/NewSakuya.obj", mat);
+
+            FrameBuffer* frameBuffer = (FrameBuffer*)NativeMemory.Alloc((nuint)sizeof(FrameBuffer)); 
+            *frameBuffer = new FrameBuffer(640, 320);
+
+            CameraMixer* cameraMixer = (CameraMixer*)NativeMemory.Alloc((nuint)sizeof(CameraMixer)); 
+            *cameraMixer = new CameraMixer();
+
+            OpenGL.cameraMixer = cameraMixer;
+            OpenGL.frameBuffer = frameBuffer;
+
+            engine.Mixer.Life.cameraMixer = cameraMixer;
+            SceneWindow.Depends(frameBuffer, cameraMixer);
 
             while (!glfw.WindowShouldClose(window))
             {
                 glfw.PollEvents();
 
-                rendering.Render(in engine.Intent);
+                engine.Device.Update();
+
+                engine.Gui.Update(in context);
+
+                engine.Renderer.Before(in context);
+                engine.Mixer.Life.Whip(in context);
+                engine.Renderer.AfterBefore(in context);
+
+                engine.Renderer.Render(in context);
+
+                engine.Gui.Render(in context);
+
+                engine.Renderer.After(in context);
 
                 engine.Intent.KickBackInvoke();
+                
                 Thread.Sleep(6);
             }
 
             engine.Dispose();
-            rendering.Dispose();
-            textureLoader.Dispose();
+
+            frameBuffer->Dispose();
+
+            NativeMemory.Free(cameraMixer);
+            NativeMemory.Free(frameBuffer);
 
             glfw.DestroyWindow(window);
             glfw.Terminate();
