06d442fb42e08d636bf518cbe98769a8d2ef3600
Zynres <killp8918@gmail.com>
2026-07-27 21:49:47 +0300

    implement: resource mangare add depends

--- src/SweetEngine/Resources/ResourceManager.cs
+++ src/SweetEngine/Resources/ResourceManager.cs
@@ -1,13 +1,37 @@
 using SweetEngine.IO.Loaders;
+using SweetEngine.Resources.Shaders;
+using SweetLib.Devices;
 
 namespace SweetEngine.Resources;
 
 public struct ResourceManager
 {
-    private readonly Texture2DLoader texture2DLoader;
+    public TextureLoader TextureLoader;
+    public ShaderLoader ShaderLoader;
+
+    public Dictionary<string, ObjLoader> MeshLoaders;
+
+    public Shader Shader;
 
     public ResourceManager()
     {
-        texture2DLoader = new();
+        ShaderLoader = new();
+        MeshLoaders = new(1);
+    }
+
+    public void Init()
+    {
+        GraphicShader shader = new();
+        Shader = new(
+            in ShaderLoader, in shader.vertexSrc, in shader.fragmentSrc);
+
+        MeshLoaders[".obj"] = new ObjLoader();
+        TextureLoader = new();
+    }
+
+    public void Dispose()
+    {
+        TextureLoader.Dispose();
+        Shader.Dispose(in ShaderLoader);
     }
 }
